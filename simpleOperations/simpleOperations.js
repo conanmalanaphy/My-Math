1ad423da1d2b42025299e5f3c82878e18cc891e2
Conan <conanmalanaphy@gmail.com>
2021-02-24 23:05:39 +0000

    intentionally fixed bug

--- simpleOperations/simpleOperations.js
+++ simpleOperations/simpleOperations.js
@@ -1,5 +1,5 @@
 const add = (a, b) => a + b
-const subtract = (a, b) => a * b
+const subtract = (a, b) => a - b
 const multiply = (a, b) => a * b
 const divide = (a, b) => a / b
 const exponent = (a, b) => a ** b
